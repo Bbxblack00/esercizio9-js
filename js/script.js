d2eaf81907720792132fb8676474c62a044c576b
Benito <bbxblack@gmail.com>
2021-03-12 10:10:51 +0100

    esercizio live

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+
+function sayHello(username, lastName) {  // le function sono mini programmi che vengono richiamati durante l'esecuzione del codice per non riscrivere altri algoritmi.
+  console.log('Hello ' + username + ' ' + lastName); // dentro alle parentesi delle function, vengono dichiarate le variabili che possono essere usate solo all'interno delle funzioni.
+}
+
+sayHello('Gustav', 'Sus');
+sayHello('Francesco', 'Lel');
